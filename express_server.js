f68f5eb1b088303e4adff0e4d43540515a857217
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-06-23 23:04:27 -0500

    log POST request and then respond with something

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {}
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -26,9 +30,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase.b2xVn2 };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
