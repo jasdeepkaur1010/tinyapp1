87d286417ed64e5879336f01557501f7dd9ec1a6
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-06-26 14:27:42 -0500

    add logout request to the server

--- express_server.js
+++ express_server.js
@@ -76,10 +76,15 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/login", (req, res) =>{
+app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie('username', username);
   res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
